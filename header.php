32dcd299873d8e93124de995dfc2e22931a5b5f2
kev1venteur <kvin.gillet@gmail.com>
2019-05-30 22:36:40 +0200

    bug

--- header.php
+++ header.php
@@ -2,13 +2,13 @@
   <body>
     <div class="header">
       <nav>
-        <div class="nav-wrapper blue lighten-1">
+        <div class="nav-wrapper blue lighten-2">
           <img class="brand-logo" src="pictures/head_only.png" alt="face-logo"></img>
           <a href="index.php" class="brand-logo center hide-on-med-and-down">
             <img class="brand-name-image" src="pictures/text_only.png" alt="text-logo"></img>
           </a>
           <ul id="nav-mobile" class="right hide-on-med-and-down">
-            <li><a href="">Sass</a></li>
+            <li><a href="">Sass  </a></li>
             <li><a href="">Components</a></li>
             <li><a href="">JavaScript</a></li>
           </ul>
