8a26a3720f18799dc913dc8f2bc782d9e4459399
Kev1venteur <kvin.gillet@gmail.com>
2019-05-30 21:48:04 +0200

    logo overflow géré

--- header.php
+++ header.php
@@ -3,7 +3,10 @@
     <div class="header">
       <nav>
         <div class="nav-wrapper">
-          <a href="index.php" class="brand-logo"><img class="brand_face_logo" src="pictures/head_only.png" alt="brand logo"></a>
+          <img class="brand-logo" src="pictures/head_only.png" alt="face-logo"></img>
+          <a href="index.php" class="center hide-on-med-and-down">
+            <img class="brand-name" src="pictures/text_only.png" alt="text-logo"></img>
+          </a>
           <ul id="nav-mobile" class="right hide-on-med-and-down">
             <li><a href="">Sass</a></li>
             <li><a href="">Components</a></li>
