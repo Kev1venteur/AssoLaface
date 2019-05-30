e6881d0038bbbaff0b017cfa187e34c7d8dde2c5
kev1venteur <kvin.gillet@gmail.com>
2019-05-30 22:17:27 +0200

    passage en bleu et ajout logo name centré

--- header.php
+++ header.php
@@ -2,10 +2,10 @@
   <body>
     <div class="header">
       <nav>
-        <div class="nav-wrapper">
+        <div class="nav-wrapper blue lighten-1">
           <img class="brand-logo" src="pictures/head_only.png" alt="face-logo"></img>
-          <a href="index.php" class="center hide-on-med-and-down">
-            <img class="brand-name" src="pictures/text_only.png" alt="text-logo"></img>
+          <a href="index.php" class="brand-logo center hide-on-med-and-down">
+            <img class="brand-name-image" src="pictures/text_only.png" alt="text-logo"></img>
           </a>
           <ul id="nav-mobile" class="right hide-on-med-and-down">
             <li><a href="">Sass</a></li>
