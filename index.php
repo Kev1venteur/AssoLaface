ad62558c0dfdc059c4b49d820dd14b7b8a5b9636
kev1venteur <kvin.gillet@gmail.com>
2019-06-05 15:42:32 +0200

    petite retouche pour accès au menu d'administration

--- index.php
+++ index.php
@@ -8,15 +8,14 @@
                 <img src="pictures/notebook.jpg">
               </div>
               <a href="#user"><img class="circle" src="pictures/head_only.png"></a>
-              <a href="#name"><span class="white-text name">Association L'AFACE</span></a>
+              <a href="#name"><span class="white-text name">Association Froidfondaise</span></a>
               <a href="#email"><span class="white-text email">associationlaface@gmail.com</span></a>
             </div>
           </li>
-          <li><a href="#!"><i class="material-icons">cloud</i>First Link With Icon</a></li>
-          <li><a href="#!">Second Link</a></li>
+          <li><a href="#!"><i class="material-icons">import_contacts</i>Historique Evénementiel</a></li>
           <li><div class="divider"></div></li>
-          <li><a class="subheader">Subheader</a></li>
-          <li><a class="waves-effect" href="#!">Third Link With Waves</a></li>
+          <li><a class="subheader">Administration</a></li>
+          <li><a href="#!"><i class="material-icons">account_circle</i>Connexion</a></li>
         </ul>
         <a href="index.php" data-target="slide-out" class="sidenav-trigger waves-effect waves-light btn brown lighten-2 hoverable hide-on-med-and-down"><i class="material-icons">arrow_forward_ios</i></a>
         <!--Fin sidenav marron et début des cartes-->
