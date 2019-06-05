6531bc457deb41a8d620cc43f821fa5772265981
kev1venteur <kvin.gillet@gmail.com>
2019-06-05 18:18:26 +0200

    corriger le login.php

--- admin/login.php
+++ admin/login.php
@@ -35,7 +35,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     // Validate credentials
     if(empty($username_err) && empty($password_err)){
         // Prepare a select statement
-        $sql = "SELECT id, username, password FROM users WHERE username = ?";
+        $sql = "SELECT idUtilisateur, loginUtilisateur, passwordUtilisateur FROM utilisateur WHERE loginUtilisateur = ?";
 
         if($stmt = mysqli_prepare($link, $sql)){
             // Bind variables to the prepared statement as parameters
@@ -62,7 +62,6 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                             $_SESSION["loggedin"] = true;
                             $_SESSION["id"] = $id;
                             $_SESSION["username"] = $username;
-
                             // Redirect user to welcome page
                             header("location: menu.php");
                         } else{
--- admin/menu.php
+++ admin/menu.php
@@ -1,4 +1,14 @@
-<?php include '../modal/header.php'; ?>
+<?php
+// Initialize the session
+session_start();
+
+// Check if the user is logged in, if not then redirect him to login page
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    header("location: login.php");
+    exit;
+}
+include '../modal/header.php';
+?>
       <main>
         <a class="waves-effect waves-light btn-large">Button</a>
         <a class="waves-effect waves-light btn-large"><i class="material-icons left">cloud</i>button</a>
