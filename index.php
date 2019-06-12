2878986705bd56cc6ebdac96161b4c1e8fdc58a6
Kev1venteur <kvin.gillet@gmail.com>
2019-06-12 03:53:19 +0200

    taill bulle info ok

--- index.php
+++ index.php
@@ -21,7 +21,7 @@ $brand = $result->fetchAll();
               <div class="card-image">
                 <img src="/assolaface/photos/brand_pictures/<?php echo $card["nomPhoto"] ?>">
                 <span class="card-title orange-text"><b><?php echo $card["nomEntreprise"] ?> <?php echo $card["domaineEntreprise"] ?></b></span>
-                <a class="btn-floating waves-effect waves-light blue modal-trigger left" href="#modal<?php echo $card["idEntreprise"] ?>"><i class="material-icons">info</i></a>
+                <a class="btn-floating btn-small waves-effect waves-light blue modal-trigger left" href="#modal<?php echo $card["idEntreprise"] ?>"><i class="material-icons">info</i></a>
                 <a href="<?php echo $card["urlSiteRedirectionCarte"] ?>" class="btn-floating halfway-fab waves-effect waves-light red" target="_blank"><i class="material-icons">chevron_right</i></a>
               </div>
               <div class="card-content">
