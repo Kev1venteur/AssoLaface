585f4832b1aea08ed47f7b690a15783170f52778
Kev1venteur <kvin.gillet@gmail.com>
2019-06-12 04:00:26 +0200

    modal pour coordonnées

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 $db = new PDO("mysql:host=" . Config::DB_SERVER . ";dbname=" . Config::DB_NAME
 , Config::DB_USERNAME, Config::DB_PASSWORD, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
 
-$sql_query = "SELECT e.nomEntreprise, e.idEntreprise, e.domaineEntreprise, e.descriptionEntreprise, p.nomPhoto, c.urlSiteRedirectionCarte
+$sql_query = "SELECT e.nomEntreprise, e.idEntreprise, e.domaineEntreprise, e.descriptionEntreprise, p.nomPhoto, c.urlSiteRedirectionCarte, c.tel, c.addresse
 FROM entreprise e JOIN photo p on p.entreprise_idEntreprise=e.idEntreprise JOIN coordonnees c on c.entreprise_idEntreprise=e.idEntreprise";
 
 $result = $db->prepare($sql_query);
@@ -16,6 +16,16 @@ $brand = $result->fetchAll();
         <?php include 'modal/sidenav.php'; ?>
         <div class="row">
           <?php foreach ($brand as $card){ ?>
+          <div id="modal<?php echo $card["idEntreprise"] ?>" class="modal">
+           <div class="modal-content">
+             <h4>Prendre Contact</h4>
+             <p>Tel: <?php echo $card["tel"] ?></p>
+             <p>Adresse: <?php echo $card["addresse"] ?></p>
+           </div>
+           <div class="modal-footer">
+             <a href="#!" class="modal-close waves-effect waves-green btn-flat">OK</a>
+           </div>
+         </div>
           <div class="col s12 m3">
             <div class="card">
               <div class="card-image">
