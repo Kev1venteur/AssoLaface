2aa1df0bd8b8ac3025c73037b78486599580f84e
kev1venteur <kvin.gillet@gmail.com>
2019-06-15 03:06:02 +0200

    meta description ok

--- modal/head.php
+++ modal/head.php
@@ -3,6 +3,7 @@
   <head>
     <meta charset="utf-8" name="viewport" content="width=device-width, initial-scale=1.0"/>
     <title>Site L'AFACE</title>
+    <meta name="description" content="Référencement et Présentation de tout les membres de l'association L'AFACE de Froidfond" />
     <link rel="stylesheet" href="/assolaface/css/style.css">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link type="text/css" rel="stylesheet" href="/assolaface/css/materialize.css"  media="screen,projection"/>
