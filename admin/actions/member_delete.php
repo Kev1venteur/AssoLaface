133ccc64fe6a07e58cc85ba1c49b4d0a98233467
kev1venteur <kvin.gillet@gmail.com>
2019-06-15 21:11:16 +0200

    member delete ok

---
+++ admin/actions/member_delete.php
@@ -0,0 +1,20 @@
+<?php
+include '../../modal/login_security.php';
+
+$idAdherent= filter_input(INPUT_GET, "idAdherent");
+
+require_once '../../config/config.php';
+
+$db = new PDO("mysql:host=" . Config::DB_SERVER . ";dbname=" . Config::DB_NAME
+, Config::DB_USERNAME, Config::DB_PASSWORD, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
+
+$sql_query = "delete from adherent where idAdherent=:idAdherent";
+
+$result = $db->prepare($sql_query);
+
+$result->bindParam(":idAdherent", $idAdherent);
+
+$result->execute();
+
+header('Location: /assolaface/admin/management/member.php');
+?>
