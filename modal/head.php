517cb11008935c7d1ad1110af40be04e3b412e7c
Kévin <kvin.gillet@gmail.com>
2019-07-25 20:03:49 +0200

    favicon

--- modal/head.php
+++ modal/head.php
@@ -4,8 +4,7 @@
     <meta charset="utf-8" name="viewport" content="width=device-width, initial-scale=1.0"/>
     <title>L'AFACE de Froidfond</title>
     <meta name="description" content="Trouvez le professionnel qu'il vous faut sur Froidfond" />
-    <link rel="icon" type="image/png" href="/pictures/head_only.png" />
-    <link rel="shortcut icon" type="image/png" href="/pictures/head_only.png">
+    <link rel="icon" type="image/png" href="/pictures/favicon.ico" />
     <link rel="stylesheet" href="/css/style.css">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link type="text/css" rel="stylesheet" href="/css/materialize.css"  media="screen,projection"/>
